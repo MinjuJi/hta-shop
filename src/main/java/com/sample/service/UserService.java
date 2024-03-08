2b38ff866948819d2529e8b4db7f92610a600214
MinjuJi <hinote444@gmail.com>
2024-03-08 11:27:29 +0900

    feat: 사용자 권한 부여 기능 구현

--- src/main/java/com/sample/service/UserService.java
+++ src/main/java/com/sample/service/UserService.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Service;
 import com.sample.exception.AlreadyUsedEmailException;
 import com.sample.exception.AlreadyUsedIdException;
 import com.sample.mapper.UserMapper;
+import com.sample.mapper.UserRoleMapper;
 import com.sample.vo.User;
+import com.sample.vo.UserRole;
 import com.sample.web.form.UserRegisterForm;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,7 @@ public class UserService {
 	// 회원가입 시 비밀번호 암호화를 위해서 PasswordEncoder 구현 객체를 주입받는다.
 	private final PasswordEncoder passwordEncoder;
 	private final UserMapper userMapper;
+	private final UserRoleMapper userRoleMapper;
 	/* 위와 동일한 코드
 	 
 		@Service
@@ -36,6 +39,19 @@ public class UserService {
 		return userMapper.getUserById(id);
 	}
 
+	/*
+	 * < 신규 회원가입 서비스를 제공하는 메소드 >
+	 * 
+	 * 	- 신규 가입 프로세스
+	 * 		1. 회원아이디로 회원정보를 조회한다. 회원정보가 존재하면 예외를 발생시킨다.
+	 * 		2. 이메일로 회원정보를 조회한다. 회원정보가 존재하면 예외를 발생시킨다.
+	 * 		3. UserRegisterForm 객체에 저장된 신규 회원정보로 User 객체를 생성한다.
+	 * 		4. 비밀번호를 암호화하고, 암호화된 비밀번호를 저장한다.
+	 * 		5. 신규 회원정보가 저장된 User 객체를 mybatis에 전달해서 저장시킨다.
+	 * 		6. 신규 회원은 ROLE_USER 권한을 부여하기 위해서 UserROle 객체를 생성해서 사용자번호와 "ROLE_USER" 객체를 대입한다.
+	 * 		7. 신규 회원의 권한 정보가 저장된 UserRole 객체를 mybatis에 전달해서 데이터베이스에 저장시킨다.
+	 * 		
+	 */
 	public void registerUser(UserRegisterForm form) {
 		User foundUser = userMapper.getUserById(form.getId());
 		
@@ -54,5 +70,23 @@ public class UserService {
 			user.setPassword(secretPassword);
 			
 			userMapper.insertUser(user);
+			
+			UserRole userRole = new UserRole();
+			userRole .setUserNo(user.getNo());
+			userRole.setRolename("ROLE_USER");
+			
+			userRoleMapper.insertUserRole(userRole);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
