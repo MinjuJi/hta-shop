6bebc9c8361a9df008860d0920ae08568e84784f
MinjuJi <hinote444@gmail.com>
2024-03-13 18:00:47 +0900

    feat: 관리자 페이지의 사용자 관리에서 상세보기 클릭 시 팝업창에 사용자 상세정보를 표시하는 기능 구현

--- src/main/java/com/sample/service/UserService.java
+++ src/main/java/com/sample/service/UserService.java
@@ -1,5 +1,7 @@
 package com.sample.service;
 
+import java.util.List;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +36,10 @@ public class UserService {
 		}
 		
 	*/
+	
+	public List<User> getAllUsers(){
+		return userMapper.getAllUsers();
+	}
 	
 	public User getUser(String id) {
 		return userMapper.getUserById(id);
---
+++ src/main/java/com/sample/web/dto/UserDto.java
@@ -0,0 +1,23 @@
+package com.sample.web.dto;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+	private int no;
+	private String id;
+	private String name;
+	private String tel;
+	private String email;
+	@JsonFormat(pattern = "M월 d일")
+	private Date birth;
+	@JsonFormat(pattern = "yyyy년 M월 d일")
+	private Date createdDate;
+}
